aed6e51bab6460a4e5207049fb9b9cd1321598c8
A-22 <finex0701@gmail.com>
2026-08-01 18:29:48 +0300

    Deploy

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/{any}', function () {
+    $indexPath = public_path('build/index.html');
+
+    if (file_exists($indexPath)) {
+        return response()->file($indexPath, ['Content-Type' => 'text/html']);
+    }
+
+    abort(404, 'Frontend build not found');
+})->where('any', '^(?!api).*$');
